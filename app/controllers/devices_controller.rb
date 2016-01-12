483cb7a275494dfbb85dbfac16d1469828c510ee
sourav27 <debnath.sourabh@gmail.com>
2016-01-12 23:39:05 +0530

    Intial commit

---
+++ app/controllers/devices_controller.rb
@@ -0,0 +1,24 @@
+class DevicesController < ApplicationController
+before_action :set_device, only: [:state_change]
+
+	def index
+		@devices = Device.all
+	end
+
+	def state_change
+		# raise @device.inspect
+		if @device.state
+			@device.update(state: false)
+		else
+			@device.update(state: true)
+		end
+		redirect_to root_url
+	end
+
+	def set_device
+		@device = Device.find(params[:id])
+	end
+
+	def show
+	end
+end
