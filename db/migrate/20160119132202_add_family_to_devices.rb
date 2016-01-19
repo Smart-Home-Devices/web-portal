4151214f5404c904dbf87f9face209adf4fd99a6
sourav27 <debnath.sourabh@gmail.com>
2016-01-19 19:17:05 +0530

    migration error fix#2

--- db/migrate/20160119132202_add_family_to_devices.rb
+++ db/migrate/20160119132202_add_family_to_devices.rb
@@ -1,6 +1,6 @@
 class AddFamilyToDevices < ActiveRecord::Migration
   def change
-  	# remove_column :devices, :family_id
+  	remove_column :devices, :family_id
   	add_column :devices, :family_id, :integer
   end
 end
