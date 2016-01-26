1c5954a7b7dd02baa5f57752d6481264343b4344
sourav27 <debnath.sourabh@gmail.com>
2016-01-26 19:53:41 +0530

    create sensors on family creation

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -4,6 +4,10 @@ def create
 	@family = Family.new(family_params)
 	respond_to do |format|
 			if @family.save
+				Sensor.create(name: "Temperature", state: true, value: 0, family_id: @family.id, rpi_id: @family.rpi_id)
+				Sensor.create(name: "Humidity", state: true, value: 0, family_id: @family.id, rpi_id: @family.rpi_id)
+				Sensor.create(name: "LPG Gas Sensor", state: true, value: 0, family_id: @family.id, rpi_id: @family.rpi_id)
+				Sensor.create(name: "CO2 Gas Sensor", state: true, value: 0, family_id: @family.id, rpi_id: @family.rpi_id)
 				format.html { redirect_to new_user_registration_path(family_id: @family.id), notice: 'New family created!' }
         		# format.json { render :show, status: :created, location: @device }
 			else
